409cd4a469bc5d2330bdaf0b8c1546848e13b20c
siddique8319 <siddique528142@gmail.com>
2019-11-28 10:34:00 +0600

    full project not complete

---
+++ resources/views/layouts/partial/topbar.blade.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+    <div class="container-fluid">
+        <div class="navbar-wrapper">
+            <a class="navbar-brand" href="#pablo">Dashboard</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#pablo">
+                        <i class="material-icons">dashboard</i>
+                        <p class="d-lg-none d-md-block">
+                            Stats
+                        </p>
+                    </a>
+                </li>
+{{--                <li>--}}
+{{--                    <a href="#pablo" class="dropdown-toggle" data-toggle="dropdown">--}}
+{{--                        <i class="material-icons">person</i>--}}
+{{--                        <p class="hidden-lg hidden-md">Profile</p>--}}
+{{--                    </a>--}}
+{{--                </li>--}}
+                <li>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        <i class="material-icons">exit_to_app</i>
+                        Logout
+                    </a>
+                    <form id="logout-form" method="POST" action="{{ route('logout') }}" style="display: none">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
